2494966a8215770ce4eca81c503ad8144a56b34d
ConsueloShelo <acaf504082@gmail.com>
2023-06-14 12:13:04 -0500

    build_funcionesMovimiento

--- grid.gd
+++ grid.gd
@@ -7,6 +7,7 @@ export (int) var x_start
 export (int) var y_start
 export (int) var offset
 
+#Array de piezas
 var possible_pieces = [
 	preload("res://Scenes/piece_two.tscn"),
 	preload("res://Scenes/piece_three.tscn"),
@@ -16,7 +17,13 @@ var possible_pieces = [
 	preload("res://Scenes/piece_ten.tscn")
 ]
 
+#Piezas en la escena
 var all_pieces = []
+
+#Interacción de las piezas
+var first_touch = Vector2(0, 0)
+var final_touch = Vector2(0, 0)
+var controlling = false
 
 func _ready():
 	randomize()
@@ -63,6 +70,50 @@ func grid_to_pixel(column, row):
 		var new_y = y_start + offset * row
 		return Vector2(new_x, new_y)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func pixel_to_grid(pixel_x, pixel_y):
+	var new_x = round(pixel_x - x_start) / offset
+	var new_y = round(pixel_y - y_start) / offset
+	return Vector2(new_x, new_y)
+
+func is_in_grid(column, row):
+	if column >= 0 && column < width:
+		if row >= 0 && row < height:
+			return true 
+	return false
+
+func touch_input():
+	if Input.is_action_just_pressed("ui_touch"):
+		first_touch = get_global_mouse_position()
+		var grid_position = pixel_to_grid(first_touch.x, first_touch.y)
+		if is_in_grid(grid_position.x, grid_position.y):
+			controlling = true
+
+	if Input.is_action_just_pressed("ui_touch"):
+		final_touch = get_global_mouse_position()
+		var grid_position = pixel_to_grid(final_touch.x, final_touch.y)
+		if is_in_grid(grid_position.x, grid_position.y) && controlling:
+			touch_difference(pixel_to_grid(first_touch.x, first_touch.y), grid_position)
+
+func swap_pieces(column, row, direction):
+	var first_piece = all_pieces[column][row]
+	var other_piece = all_pieces[column + direction.x][row + direction.y]
+	all_pieces[column][row] = other_piece
+	all_pieces[column + direction.x][row + direction.y] = first_piece
+	first_piece.position = grid_to_pixel(column + direction.x, row + direction.y)
+	other_piece.position = grid_to_pixel(column, row)
+
+func touch_difference(grid_1, grid_2):
+	var difference = grid_2 - grid_1
+	if abs(difference.x) > abs(difference.y):
+		if difference.x > 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(1, 0))
+		elif difference.x < 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(-1, 0))
+	elif abs(difference.y) > abs(difference.x):
+		if difference.y > 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(0, 1))
+		elif difference.y < 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(0, -1))
+
+func _process(delta):
+	touch_input()
