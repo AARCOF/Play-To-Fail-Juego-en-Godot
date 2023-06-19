a5665e068a5de0d8ee928a1493c32c5abfdecb7c
ConsueloShelo <acaf504082@gmail.com>
2023-06-19 08:08:52 -0500

    build_animationPieces

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -1,18 +1,18 @@
 extends Node2D
-
+var column: float = 0
+var row: float = 0
+#var vec = Vector2(column, row)
 export (String) var color
 var move_tween
-var matched = false
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	move_tween = get_node("move_tween")
 
 func move(target):
-	move_tween.interpolate.property(self, "position", position, target, .3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-	move_tween.start()
+	
+	move_tween.interpolate_property(self, "position", position, target, 0.3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func dim():
-	var sprite = get_node("Sprite")
-	sprite.modulate = Color(1, 1, 1, 0.5)
