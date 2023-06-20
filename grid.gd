eb54e3a60acf02d87d43e3bcc813e3b973953c85
ConsueloShelo <acaf504082@gmail.com>
2023-06-20 07:57:24 -0500

    build_deleteMatched

--- grid.gd
+++ grid.gd
@@ -81,25 +81,23 @@ func pixel_to_grid(pixel_x, pixel_y):
 	var new_y = round((pixel_y - y_start) / offset)
 	return Vector2(new_x, new_y)
 
-func is_in_grid(column, row):
-	if column >= 0 && column < width:
-		if row >= 0 && row < height:
+func is_in_grid(grid_position):
+	if grid_position.x >= 0 && grid_position.x < width:
+		if grid_position.y >= 0 && grid_position.y < height:
 			return true
 	return false
 
 func touch_input():
 	if Input.is_action_just_pressed("ui_touch"):
-		first_touch = get_global_mouse_position()
-		var grid_position = pixel_to_grid(first_touch.x, first_touch.y)
-		if is_in_grid(grid_position.x, grid_position.y):
+		if is_in_grid(pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y)):
+			first_touch = pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y)
 			controlling = true
-
 	if Input.is_action_just_released("ui_touch") && controlling:
-		final_touch = get_global_mouse_position()
-		var grid_position = pixel_to_grid(final_touch.x, final_touch.y)
-		if is_in_grid(grid_position.x, grid_position.y):
-			touch_difference(pixel_to_grid(first_touch.x, first_touch.y), grid_position)
+		if is_in_grid(pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y)) && controlling:
 			controlling = false
+			final_touch = pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y )
+			touch_difference(first_touch, final_touch)
+			
 
 func swap_pieces(column, row, direction):
 	var first_piece = all_pieces[column][row]
@@ -154,4 +152,14 @@ func find_matches():
 							all_pieces[i][j].matched = true
 							all_pieces[i][j].dim()
 							all_pieces[i][j + 1].matched = true
-							all_pieces[i][j + 1].dim()	
+							all_pieces[i][j + 1].dim()
+	get_parent().get_node("destroy_timer").start()
+func destroy_matched():
+	for i in range(width):
+		for j in range(height):
+			if all_pieces[i][j] != null:
+				if all_pieces[i][j].matched:
+					all_pieces[i][j].queue_free()
+
+func _on_destroy_timer_timeout():
+	destroy_matched()
